4f9368cdc599266e9a78400057678a330503de0e
David Etuk <djetuk@gmail.com>
2023-06-08 02:16:02 +0100

    feat: built the banner component

---
+++ src/component/utility/banner.component.tsx
@@ -0,0 +1,12 @@
+const Banner = () => {
+  return (
+    <div className="grid grid-cols-6 justify-items-center py-12 pl-[4rem] text-center">
+      <div className="text-5xl col-start-2 col-span-4 font-thin text-gray-600">
+        Providing you with a world wide{" "}
+        <span className="uppercase text-6xl font-bold">weather forecast</span>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
