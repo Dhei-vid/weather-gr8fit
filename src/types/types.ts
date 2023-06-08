7afe34da09b2c7ae3fca10ec29f1b2e7a170364b
David Etuk <djetuk@gmail.com>
2023-06-08 02:14:25 +0100

    feat: handling all types in one file

---
+++ src/types/types.ts
@@ -0,0 +1,19 @@
+import { ChangeEventHandler, KeyboardEventHandler } from "react";
+
+export type WeatherInfomation = {
+  name: string;
+  weather: [{ description: string }];
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  main: { temp: number; pressure: number; humidity: number };
+  wind: { speed: number };
+};
+
+export type SearchBoxProps = {
+  location: string;
+  placeholder?: string;
+  onChangeHandler: ChangeEventHandler<HTMLInputElement>;
+  onKeyDown: KeyboardEventHandler<HTMLInputElement>;
+};
