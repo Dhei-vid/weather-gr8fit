2642440a629353b1c1407ee5dbc686ffb47ffcd5
David Etuk <djetuk@gmail.com>
2023-06-11 23:12:38 +0100

    fix: moved from default export to named export

--- src/component/api/api.component.ts
+++ src/component/api/api.component.ts
@@ -1,11 +1,9 @@
 type dataURLType = string;
 
-const getData = async <T>(url: dataURLType): Promise<T> => {
+export const getData = async <T>(url: dataURLType): Promise<T> => {
   const data = await fetch(url);
 
   const response = await data.json();
 
   return response;
 };
-
-export default getData;
